461a8be81a60c3ed968d7e1346e1dacd7692901b
Raw-fish <83627885+Raw-fish@users.noreply.github.com>
2023-01-03 10:14:52 +0800

    Add files via upload

---
+++ src/org/fkit/controller/Hello.java
@@ -0,0 +1,15 @@
+package org.fkit.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class Hello {
+	//这里代表网站后面什么都不加，跳转到主页
+	@RequestMapping(value="")
+    public String main(Model model) {
+    	model.addAttribute("message","hello");
+    	return "main";
+    }
+}
