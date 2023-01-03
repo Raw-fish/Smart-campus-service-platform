e98c494c24343a40e1093282622c518254b92acf
Raw-fish <83627885+Raw-fish@users.noreply.github.com>
2023-01-03 10:16:29 +0800

    Update Hello.java

--- src/org/fkit/controller/Hello.java
+++ src/org/fkit/controller/Hello.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class Hello {
 	//这里代表网站后面什么都不加，跳转到主页
+	//对应WebContent\WEB-INF\content\main.jsp
 	@RequestMapping(value="")
     public String main(Model model) {
     	model.addAttribute("message","hello");
